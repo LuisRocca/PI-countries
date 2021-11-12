d552b63c5d92486f4fbd0fca0859742f3eb49e6d
luisRocca <luis.rocca96@gmail.com>
2021-11-12 16:10:46 -0500

    listoslistos

--- api/tests/models/country.spec.js
+++ api/tests/models/country.spec.js
@@ -1,7 +1,7 @@
-const { Country, conn } = require('../../src/db.js');
+const { Country, Activity , conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
-describe('Country model', () => {
+xdescribe('Country model', () => {
   before(() => conn.authenticate()
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
@@ -14,9 +14,56 @@ describe('Country model', () => {
           .then(() => done(new Error('It requires a valid name')))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Country.create({ name: 'Argentina' });
+     
+     
+      it('debería funcionar cuando es un nombre válido', () => {
+        Country.create({ name: 'Venezuela' });
       });
     });
+    describe('id como lo pide el readme', () => {
+      it('debería funcionar cuando es un id válido', () => {
+        Country.create({ id: 'COL' });
+      });
+      it('debería funcionar cuando es un id válido', () => {
+        Country.create({ id: 'ARG' });
+      });
+      it('debería funcionar cuando es un id válido', () => {
+        Country.create({ id: 'BOL' });
+      });
+      it('debería funcionar cuando es un id válido', () => {
+        Country.create({ id: 'CHI' });
+      });
+    })
+    describe('countinents como lo pide el readme', () => {
+      it('se supone que arroje true si el continente existe', () => {
+        Country.create({  continents: 'North America' });
+      });
+      it('se supone que arroje true si el continente existe', () => {
+        Country.create({  continents: 'Europe' });
+      });
+      it('se supone que arroje true si el continente existe', () => {
+        Country.create({  continents: 'Africa' });
+      });
+      it('se supone que arroje true si el continente existe', () => {
+        Country.create({  continents: 'Ocea' });
+      });
+    })
   });
 });
+
+xdescribe('Activity model', () => {
+  before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+  describe('Validators 2', () => {
+    beforeEach(() => Activity.sync({ force: true }));
+    describe('todos los valores', () => {
+      it('should throw an error if name is null', (done) => {
+        Country.create({})
+          .then(() => done(new Error('It requires a valid name')))
+          .catch(() => done());
+      });
+    })
+  })
+})
--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -1,24 +1,93 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Country, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Country, conn } = require("../../src/db.js");
 
 const agent = session(app);
+
 const country = {
-  name: 'Argentina',
+  name: "Argentina",
 };
 
-describe('Country routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Country.sync({ force: true })
-    .then(() => Country.create(pokemon)));
-  describe('GET /countries', () => {
-    it('should get 200', () =>
-      agent.get('/countries').expect(200)
-    );
+describe("Country routes (testing de las rutas GET)", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  // beforeEach(() => Country.sync({ force: true })
+  //   .then(() => Country.create(pokemon)));
+  describe("GET /countries", () => {
+    it("should get 200", () =>
+      agent.get("/countries")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      );
+  });
+  xdescribe("GET /countries?name (este es para el searh)", () => {
+    it("este es el des query", () =>
+      agent
+        .get("/countries?name=argentina")
+        .expect(200)
+        .expect("Content-Type", /json/));
+    it("este es el des query", () =>
+      agent
+        .get("/countries?name=ARGENTINA")
+        .expect(200)
+        .expect("Content-Type", /json/));
+    it("este es 404", () =>
+      agent
+        .get("/countries?name=argzz")
+        .expect(404)
+        .expect("Content-Type", /json/)
+        .expect({ error: 'no se encontro ningun pais' }));
+    it("este es 404", () =>
+      agent
+        .get("/countries?name=ARGklk")
+        .expect(404)
+        .expect("Content-Type", /json/)
+        .expect({ error: 'no se encontro ningun pais' }));
+  });
+  xdescribe("GET /countries:id (traemos por id para el modal)", () => {
+    it("este texteo es a los id de los paises ", () =>
+      agent.get("/countries/VEN").expect(200).expect("Content-Type", /json/));
+    it("este texteo es a los id de los paises ", () =>
+      agent.get("/countries/ARG").expect(200).expect("Content-Type", /json/));
+    it("este retorna 404 ", () =>
+      agent.get("/countries/AAA")
+      .expect(404)
+      .expect('id no valido')
+      );
+    it("este retorna 404 ", () =>
+      agent.get("/countries/AA")
+      .expect(404)
+      .expect('id no valido')
+      );
+  });
+  xdescribe("GET /activity ", () => {
+    it("si le haces un get aca esta vacio", () => 
+    agent.get("/activity")
+    .expect(200)   
+    .expect("Content-Type", /json/)
+    .expect([])
+    )
   });
 });
+
+xdescribe("pruebas de la ruta POST ", () => {
+  it("creamos un post y responde 200", () => 
+  agent.post("/activity")
+  .send(
+    {
+      name: "Dias de qe eeq eqweqeq e zona",
+       difficulty: "1",
+       duration: "72Horas",
+       season: "Verano",
+       countriesId : ["VEN", "ARG" ]
+    }
+  )
+  .expect(200)
+  .expect('Content-Type', /json/)
+  )
+})
